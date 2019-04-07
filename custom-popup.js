951277852ef8d003061f4204083bcdb79edf94e9
muhammadawais <awaisshaikh94@gmail.com>
2019-04-07 22:44:41 +0500

    main strucutre of popup done with class structures

---
+++ custom-popup.js
@@ -0,0 +1,8 @@
+
+function showPopup() {
+    document.querySelector(".popup").classList.add("show");
+}
+
+function closePopup() {
+    document.querySelector(".popup").classList.remove("show");
+}
